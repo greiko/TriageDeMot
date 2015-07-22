a523463e44083ce1be80cd1e9298ee7f64774889
greiko <nicolas.papanicolaou20@gmail.com>
2015-07-22 19:11:19 -0400

    Version 2.0 A faire:   - Faire du clean up, optimisation

--- gestionStatistique.h
+++ gestionStatistique.h
@@ -11,24 +11,73 @@
 
 typedef struct statistique *Stats_t;
 
+/*
+ * Initialisation d'une structure statistique.
+ * Valeur de retour: Pointeur sur un structure statistique avec de la memoire alloue.
+ */
 Stats_t initiationStats(Stats_t stats);
 
+/*
+ * Initiation d'un tableau de integer a 0.
+ * Valeur de retour: Le tableau iniatilise.
+ */
 int *initiationTableauLettre();
 
+/*
+ * Compte le nombre de lettre dans le mot pour l'ajouter dans les statistiques.
+ * stats: Les statistiques.
+ * mot: Le mot utilise.
+ */
 void compteurLettre(Stats_t stats, char *mot);
 
+/*
+ * Compte le mot sans doublons et prend des donnees sur le mot.
+ * stats: Les statistiques.
+ * mot: Le mot utilise.
+ */
 void compteurMotSansDoublons(Stats_t stats, char *mot);
 
+/*
+ * Compte tout les mots, dans le fichier statistique, on le nomme:
+ * le nombre de mots avec les doublons.
+ * stats: Les statistiques.
+ */
 void compteurMotTotal(Stats_t stats);
 
+/*
+ * Compte le nombre de ligne dans le fichier entrant.
+ * stats: Les statistiques.
+ * combien: Combien de ligne a ajouter.
+ */
 void compteurLignes(Stats_t stats, int combien);
 
+/*
+ * Calcul les frequences de les lettres dans le fichier.
+ * stats: Les statistiques.
+ * lettre: Lettre se trouvant dans le mot.
+ */
 void frequenceLettre(Stats_t stats, char lettre);
 
+/*
+ * Retourne la lettre la plus frequente.
+ * stats: Les statistiques.
+ * Valeur de retour: La lettre la plus frequente.
+ */
 char lettreFrequente(Stats_t stats);
 
+/*
+ * Inscris dans le fichier les statistiques rassembler pendant le programme.
+ * stats: Les statistiques.
+ * nomDuFichier: Le nom du fichier de statistiques.
+ */
 void afficherStats(Stats_t stats, char *nomDuFichier);
 
+/*
+ * Validation lors de l'ouverture du fichier de statistiques.
+ * fichier: Le pointeur vers le fichier.
+ * nomDuFichier: Le path du fichier de statistiques.
+ * Valeur de retour: Le fichier ouvert.
+ */
 FILE *validationFichierSortie(FILE *fichier, char *nomDuFichier);
 
 #endif //TP2_GESTIONSTATISTIQUE_H
--- listeChaine.h
+++ listeChaine.h
@@ -20,18 +20,67 @@ typedef enum
   AUCUN, AVEC, SANS
 } Option_t;
 
+/*
+ * Cree ma structure listeChaine muni d'un mot.
+ * premierMot : Le mot a mettre dans Liste_t
+ * stats : Pour generer des statistiques
+ * Valeur de Retour: Un pointeur vers ce mot.
+ */
 Liste_t creeMot(char *premierMot, Stats_t stats);
 
+/*
+ * Compare deux mots pour savoir leur ordre alphabetique.
+ * motCourant : Le mot courant qui est comparer avec le nouveau mot dans la liste.
+ * nouveauMot : Le nouveau mot dans la liste.
+ * Valeur de retour : TRUE si changement doit etre fait,
+ *                    FALSE si aucun changement,
+ *                    SAME si c'est le meme mot.
+ */
 Boolean_t comparaisonMot(char *motCourant, char *nouveauMot);
 
+/*
+ * Place le nouveau mot a la place du mot courant.
+ * courant : Le mot courant.
+ * nouveauMot : Le nouveau mot.
+ * Valeur de Retour: Retourne le pointeur vers le nouveau mot.
+ */
 Liste_t placerLeMot(Liste_t courant, Liste_t nouveauMot);
 
+/*
+ * Ajoute un mot dans la liste chaine.
+ * liste : Une liste chaine.
+ * mot : Un mot a ajouter dans la liste chaine.
+ * stats : Statistique a generer.
+ * Valeur de retour : Pointeur vers le debut de la liste chaine.
+ */
 Liste_t ajouterMot(Liste_t liste, char mot[TAILLE_MOT_MAX], Stats_t stats);
 
+/*
+ * Calcule la longueur la plus courte entre le mot courant et le nouveau mot.
+ * courant : Le mot courant.
+ * nouveauMot : Le nouveau mot.
+ * Valeur de retour: La longueur le plus court.
+ */
 int longueur(char *courant, char *nouveauMot);
 
+/*
+ * Determine la facon de placer le nouveau mot dans la liste.
+ * echanger : TRUE, FALSE ou SAME.
+ * stats : Pour generer des statistiques.
+ * courant : Le mot courant.
+ * nouveauMot : Le nouveau mot.
+ * debutListe : Pointeur vers le debut de la liste chaine.
+ * Valeur de retour: Pointeur vers le debut de la liste chaine.
+ */
 Liste_t placementMot(Boolean_t echanger, Stats_t stats, Liste_t courant, Liste_t nouveauMot, Liste_t debutListe);
 
+/*
+ * Affiche le resultat du programme, les mot trie alphabetiquement sans doublons.
+ * liste : La liste chaine.
+ * stats : Les statistiques de la liste.
+ * avecStats : Si l'utilisateur le veut avec statistiques.
+ * nomDuFichier : Le nom du fichier qui contiendra les statistiques.
+ */
 void afficherListe(Liste_t liste, Stats_t stats, Option_t avecStats, char *nomDuFichier);
 
 
